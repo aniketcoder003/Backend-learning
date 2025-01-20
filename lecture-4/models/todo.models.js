d8e039fcdc24266ec17f1f372d972ff61449abe0
aniketcoder003 <aniketcoder003@gmail.com>
2025-01-20 10:36:58 +0530

    backend setup

--- lecture-4/models/todo.models.js
+++ lecture-4/models/todo.models.js
@@ -2,19 +2,19 @@ import mongoose from "mongoose";
 import { subtodo } from "./subtodo.models";
 
 const todoSchema= new mongoose.Schema({
-               createdBy:{
+                
+	           createdBy:{
 				type:mongoose.Schema.Types.ObjectId,
 				ref:'user'
 			   },
+			   
 			   subtodo:[
 				{
 					type:mongoose.Schema.Types.ObjectId,
 					ref:'subtodo'
 				}
 			   ]
-			      
-               
-                
+			                   
 })
 
 export const todo=mongoose.model('todo',todoSchema);
